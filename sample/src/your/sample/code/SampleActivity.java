2d37a0a0f877415ed4d316b0ac6075e04b020fb1
sheil <sheil@ualberta.ca>
2012-02-07 18:24:14 -0700

    yey

--- sample/src/your/sample/code/SampleActivity.java
+++ sample/src/your/sample/code/SampleActivity.java
@@ -1,27 +1,43 @@
 package your.sample.code;
 
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
-public class SampleActivity {
-	
-	
-	public static void main(String[] args)  {
+
+
+public class SampleActivity extends Activity{
+	 public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			    
+			    
+			setContentView(R.layout.main);
+			((EditText)findViewById(R.id.texty)).setText("");
+			this.main();
+	 }
+	 
+	public  void main()  {
 		long nums[] = {632528420672538449L, 429290590650920920L, 184567231788315850L};
 		for(long n : nums)
 	        decode(n, 100);
 	}
 	 
-	public static void decode(long num,int x){
+	public void decode(long num,int x){
 		for(;num>=1;num /= 1000){
 		    x += dig(num%1000)==9?((x/dig(num%1000%100))*-1) : dig(num%1000)==8?((x/dig(num%1000%100))):dig(num%1000)%2==0?(dig(num%1000%100)):(dig(num%1000%100)*-1);
-		    System.out.print((dig(num%1000%10)!=0)?(char)x:"");
-		    }
+		   Log.i("FUN", ((dig(num%1000%10)!=0)?(char)x:"").toString());
+		}
 	}
-	public static long dig(long n){
+	public  long dig(long n){
 		if(n/10<1)
 		    return n;
 		else
 		    return dig(n/10);
 	}
 	
+	public void print (Object s){
+		//((EditText)findViewById(R.id.texty)).append(s.toString());
+	}
 }
